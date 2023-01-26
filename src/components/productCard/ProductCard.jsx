afc7d4de1212031c63cc2bb8bf7cb530891b5137
Gabriel M. Wolf <92558787+gwolf97@users.noreply.github.com>
2023-01-26 16:32:23 -0500

    Card styles

--- src/components/productCard/ProductCard.jsx
+++ src/components/productCard/ProductCard.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
+import React,{ useState } from 'react'
 import "./productCard.css"
 
 const ProductCard = ({brand, gallery, id, inStock, name, prices}) => {
+
+  const [showCartBtn, setShowCartBtn] = useState(false)
+
+  const handleMouseOver = () => {
+    setShowCartBtn(true)
+  }
+
+  const handleMouseOut = () => {
+    setShowCartBtn(false)
+  }
+
   return (
-    <div className='card'>
+    <div onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} className='card'>
         <div className='img-container'>
           {!inStock && (<div className='out-of-stock-overlay'>out of stock</div>)}
           <img src={gallery[0]} alt={name} />
         </div>
-        <div className='card-cart-button'>
-          
-        </div>
+        {showCartBtn && inStock && (
+          <div className='card-cart-button'>
+            <img src="./images/white-cart.png" alt="cart" />
+              <div className="wheels">
+                <img src="./images/white-wheel.png" alt="wheel" />
+                <img src="./images/white-wheel.png" alt="wheel" />
+              </div>
+          </div>
+        )}
         <div className='title-price-container'>
           <p className='title'>{brand} {name}</p>
           <p className='price'>{prices[0].currency.symbol}{prices[0].amount}</p>
